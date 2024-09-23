c2a6f8ecfe5397d9a23140fb7c194942a6a84e1c
Giridhar Sreedhara <giridharsreedhara@Projects-MacBook-Pro.local>
2024-09-23 00:02:43 -0400

    removed logging as its not working as expected

--- SNOW2CSV.py
+++ SNOW2CSV.py
@@ -18,23 +18,6 @@ import traceback
 import os
 import numpy as np
 
-import logging
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
-
-stdout_handler = logging.StreamHandler(sys.stdout)
-stdout_handler.setLevel(logging.DEBUG)
-stdout_handler.setFormatter(formatter)
-
-file_handler = logging.FileHandler('SNOW2CSV_20240922.log')
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
-
-
-logger.addHandler(file_handler)
-logger.addHandler(stdout_handler)
 
 # This function Converts String into "Binary" (Used for converting Encrypted String "Byte" SQL Server data type to Snowflake "Binary" data)
 def String2Binary(string_value):
@@ -62,22 +45,22 @@ def CSV_2_Snowflake_Table(csv_s, folder_s, tbl_d, db_d):
     con_destination = snowflake.connector.connect(user='srvc_usr_etl_batch_load', password='ProjectX123@', account='al57695.ca-central-1.aws',
         warehouse='COMPUTE_WH', database= db_d, schema='TEST', role='DBADMIN')
     cur= con_destination.cursor()
-    logger.info("----------------------------------------------------------------------------------")
-    logger.info("  Source Folder: ", folder_s, " - CSV File: ", csv_s, '\n')
+    print("----------------------------------------------------------------------------------")
+    print("  Source Folder: ", folder_s, " - CSV File: ", csv_s, '\n')
     cursor = con_destination.cursor()
     #get identity and nullable columns
     #create an empty_list
     nullable_df = ReadNullable_list(cur, tbl_d, db_d)
     nullable_list = nullable_df['COLUMN_NAME'].tolist()
     if not nullable_list:
-        logger.info("List is empty. Creating a dummy list")
+        print("List is empty. Creating a dummy list")
         nullable_list=['empty']
     
     #empty_list
     identity_df = ReadIdentity_list(cur,tbl_d,db_d)
     identity_list = identity_df['COLUMN_NAME'].tolist()
     if not identity_list:
-        logger.info("List is empty. Creating a dummy list")
+        print("List is empty. Creating a dummy list")
         identity_df=['empty']
     
 
@@ -88,7 +71,7 @@ def CSV_2_Snowflake_Table(csv_s, folder_s, tbl_d, db_d):
         SourceData= pd.read_csv('/Users/giridharsreedhara/Desktop/'+ folder_s + '/'+ csv_s, na_filter=False)
         # Get number of records in Source CSV file
         nrows_s =len(SourceData)
-        logger.info("  - CSV File Name     : ", csv_s, " - Records: ", nrows_s)
+        print("  - CSV File Name     : ", csv_s, " - Records: ", nrows_s)
         
         # Change columns name in dataframe to uppercase - Fixing snowflake connector issue "invalid identifier from pandas dataframe"
         SourceData.columns = map(lambda x: str(x).upper(), SourceData.columns)
@@ -98,8 +81,8 @@ def CSV_2_Snowflake_Table(csv_s, folder_s, tbl_d, db_d):
 
         # Replace NaN values with '' for object data types columns that has names without the sufix "DT", "_TS" or "_DATE" (ie. is not a DATE or TIMESTAMP) 
         # This means replace NaN values with '' for String columns - Fixing snow flake load issue "NULL result in a non-nullable column"
-        str_columns = [col for col in SourceData.columns if col.endswith(('_DT', '_TS', '_DATE')) and SourceData[col].dtype == 'object']
-        SourceData[str_columns] = SourceData[str_columns].fillna('2999-12-31')
+        str_columns_TS = [col for col in SourceData.columns if col.endswith(('_DT', '_TS', '_DATE')) and SourceData[col].dtype == 'object']
+        SourceData[str_columns_TS] = SourceData[str_columns_TS].fillna('2999-12-31')
 
         SourceData = SourceData.replace(r'^\s*$', np.nan, regex=True)
         #print(SourceData.head(10))
@@ -113,7 +96,7 @@ def CSV_2_Snowflake_Table(csv_s, folder_s, tbl_d, db_d):
         if '_TS' in last_col:
                 SourceData[last_col].replace(np.nan, r'2999-12-31', regex=True, inplace=True)
         
-        for col in str_columns:
+        for col in str_columns_TS:
                 SourceData[col].replace(np.nan, r'2999-12-31', regex=True, inplace=True)
                 #SourceData[SourceData.iloc[:,-1:]] =  SourceData[SourceData.iloc[:,-1:]].fillna('')
         
@@ -129,9 +112,9 @@ def CSV_2_Snowflake_Table(csv_s, folder_s, tbl_d, db_d):
         for col in str_columns:
             if col in nullable_list:
                 SourceData
-                SourceData.col = SourceData.col.fillna('')
+                SourceData[col] = SourceData[col].fillna('')
         
-        logger.info(SourceData.head(10))
+        print(SourceData.head(10))
         
         #print("Source data after replacing Blank with NaN")
         #print(SourceData)
@@ -140,10 +123,10 @@ def CSV_2_Snowflake_Table(csv_s, folder_s, tbl_d, db_d):
         encrypted_columns = [col for col in SourceData.columns if col.endswith(('_ENCRPTD')) and SourceData[col].dtype == 'object']
         for column in encrypted_columns:
             SourceData[column] = SourceData[column].apply(String2Binary)
-        logger.info(SourceData.head(20))
+        print(SourceData.head(20))
         # Write the table data from the DataFrame to Snowflake table (with overwrite or append mode)
         success, nchunks, nrows, output= write_pandas(con_destination, SourceData, tbl_d, database=db_d, schema='TEST', auto_create_table=False, overwrite=True)
-        for col in str_columns:
+        for col in str_columns_TS:
             cur.execute(F"UPDATE {db_d}.TEST.{tbl_d} SET  {col}=NULL where {col}='2999-12-31'")
         # Get number of records in Snowflake table after refreshing it
         nrows_d = pd.read_sql_query(f'SELECT count(*) FROM {db_d}.TEST.'+tbl_d, con_destination).iloc[0][0]
@@ -152,18 +135,18 @@ def CSV_2_Snowflake_Table(csv_s, folder_s, tbl_d, db_d):
         ex_type, ex_value, ex_traceback = sys.exc_info() 
         ex_message = str(ex_value).replace('002023 (22000): SQL compilation error:\n', '').replace('100072 (22000):', '')
     finally:
-        logger.info("  - Staging output    : ", output)
-        logger.info("  - Destination Table : ", tbl_d, " - Records: ", nrows_d)
+        print("  - Staging output    : ", output)
+        print("  - Destination Table : ", tbl_d, " - Records: ", nrows_d)
         if nrows_s == nrows_d:
             msg_record_not_loaded = ""
         else :    
-            logger.error("\n  - Data Load Error   : %s" %ex_message)
+            print("\n  - Data Load Error   : %s" %ex_message)
             msg_record_not_loaded = "( " + str(nrows_s - nrows_d) + " records not loaded )"
-            logger.error(msg_record_not_loaded)
+            print(msg_record_not_loaded)
         if nrows_d <1 or nrows_s <0:
-            logger.error("Nothing to Load")
+            print("Nothing to Load")
         else:
-            logger.error("\n  -** % Records Loaded: ",  round(nrows_d/nrows_s*100, 2), "%", msg_record_not_loaded)
+            print("\n  -** % Records Loaded: ",  round(nrows_d/nrows_s*100, 2), "%", msg_record_not_loaded)
             
 
         con_destination.close()
